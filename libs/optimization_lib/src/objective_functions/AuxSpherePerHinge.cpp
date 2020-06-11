4e137f031548809cc3e65c69082f1b2c8c524ee1
Elias <elias.jadon1@gmail.com>
2020-06-11 19:01:47 +0300

    hess: step2 (done)

--- libs/optimization_lib/src/objective_functions/AuxSpherePerHinge.cpp
+++ libs/optimization_lib/src/objective_functions/AuxSpherePerHinge.cpp
@@ -448,150 +448,86 @@ void AuxSpherePerHinge::hessian() {
 		}	
 	}
 
-	////Energy 2: per face
-	//for (int fi = 0; fi < restShapeF.rows(); fi++) {
-	//	
-	//	Eigen::Matrix<double, 3, 3> dE_dx;
-	//	dE_dx <<
-	//		4 * (CurrN.row(fi).squaredNorm() - 1) + 8 * pow(CurrN(fi, 0), 2), 8 * CurrN(fi, 0)*CurrN(fi, 1)										, 8 * CurrN(fi, 0)*CurrN(fi, 2),
-	//		8 * CurrN(fi, 0)*CurrN(fi, 1)									, 4 * (CurrN.row(fi).squaredNorm() - 1) + 8 * pow(CurrN(fi, 1), 2)	, 8 * CurrN(fi, 1)*CurrN(fi, 2),
-	//		8 * CurrN(fi, 0)*CurrN(fi, 2)									, 8 * CurrN(fi, 1)*CurrN(fi, 2)										, 4 * (CurrN.row(fi).squaredNorm() - 1) + 8 * pow(CurrN(fi, 2), 2);
-	//	dE_dx *= w2;
+	//Energy 2: per face
+	for (int fi = 0; fi < restShapeF.rows(); fi++) {
+		int x0 = restShapeF(fi, 0);
+		int x1 = restShapeF(fi, 1);
+		int x2 = restShapeF(fi, 2);
+		Eigen::RowVector3d x = ((CurrV.row(x0) + CurrV.row(x1) + CurrV.row(x2)) / 3);
+		Eigen::RowVector3d c = CurrCenter.row(fi);
+		double r = CurrRadius(fi);
+		double sqrtE = (x - c).squaredNorm() - pow(r, 2);
 
-	//	for (int xyz1 = 0; xyz1 < 3; ++xyz1) {
-	//		for (int xyz2 = 0; xyz2 < 3; ++xyz2) {
-	//			int Grow = fi + (3 * restShapeV.rows()) + (xyz1 * restShapeF.rows());
-	//			int Gcol = fi + (3 * restShapeV.rows()) + (xyz2 * restShapeF.rows());
-	//			if (Grow <= Gcol) {
-	//				II.push_back(Grow);
-	//				JJ.push_back(Gcol);
-	//				SS.push_back(dE_dx(xyz1, xyz2));
-	//			}
-	//		}
-	//	}
-	//}
+		Eigen::Matrix<double, 1, 13> g_sqrtE;
+		g_sqrtE <<
+			(2 / 3.0f)*(x(0) - c(0)), // V0x
+			(2 / 3.0f)*(x(1) - c(1)), // V0y
+			(2 / 3.0f)*(x(2) - c(2)), // V0z
+			(2 / 3.0f)*(x(0) - c(0)), // V1x
+			(2 / 3.0f)*(x(1) - c(1)), // V1y
+			(2 / 3.0f)*(x(2) - c(2)), // V1z
+			(2 / 3.0f)*(x(0) - c(0)), // V2x
+			(2 / 3.0f)*(x(1) - c(1)), // V2y
+			(2 / 3.0f)*(x(2) - c(2)), // V2z
+			-2 * (x(0) - c(0)), // Cx
+			-2 * (x(1) - c(1)), // Cy
+			-2 * (x(2) - c(2)), // Cz
+			-2 * r; //r
+		Eigen::Matrix<double, 13, 13> H_sqrtE;
+		H_sqrtE <<
+			2 / 9.0f, 0, 0, 2 / 9.0f, 0, 0, 2 / 9.0f, 0, 0, -2 / 3.0f, 0, 0, 0,
+			0, 2 / 9.0f, 0, 0, 2 / 9.0f, 0, 0, 2 / 9.0f, 0, 0, -2 / 3.0f, 0, 0,
+			0, 0, 2 / 9.0f, 0, 0, 2 / 9.0f, 0, 0, 2 / 9.0f, 0, 0, -2 / 3.0f, 0,
+			2 / 9.0f, 0, 0, 2 / 9.0f, 0, 0, 2 / 9.0f, 0, 0, -2 / 3.0f, 0, 0, 0,
+			0, 2 / 9.0f, 0, 0, 2 / 9.0f, 0, 0, 2 / 9.0f, 0, 0, -2 / 3.0f, 0, 0,
+			0, 0, 2 / 9.0f, 0, 0, 2 / 9.0f, 0, 0, 2 / 9.0f, 0, 0, -2 / 3.0f, 0,
+			2 / 9.0f, 0, 0, 2 / 9.0f, 0, 0, 2 / 9.0f, 0, 0, -2 / 3.0f, 0, 0, 0,
+			0, 2 / 9.0f, 0, 0, 2 / 9.0f, 0, 0, 2 / 9.0f, 0, 0, -2 / 3.0f, 0, 0,
+			0, 0, 2 / 9.0f, 0, 0, 2 / 9.0f, 0, 0, 2 / 9.0f, 0, 0, -2 / 3.0f, 0,
+			-2 / 3.0f, 0, 0, -2 / 3.0f, 0, 0, -2 / 3.0f, 0, 0, 2, 0, 0, 0,
+			0, -2 / 3.0f, 0, 0, -2 / 3.0f, 0, 0, -2 / 3.0f, 0, 0, 2, 0, 0,
+			0, 0, -2 / 3.0f, 0, 0, -2 / 3.0f, 0, 0, -2 / 3.0f, 0, 0, 2, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -2;
 
-	////Energy 3: per face
-	//for (int fi = 0; fi < restShapeF.rows(); fi++) {
-	//	int x0 = restShapeF(fi, 0);
-	//	int x1 = restShapeF(fi, 1);
-	//	int x2 = restShapeF(fi, 2);
-	//	Eigen::Matrix<double,3,1> e21 = CurrV.row(x2) - CurrV.row(x1);
-	//	Eigen::Matrix<double, 3, 1> e10 = CurrV.row(x1) - CurrV.row(x0);
-	//	Eigen::Matrix<double, 3, 1> e02 = CurrV.row(x0) - CurrV.row(x2);
-	//	Eigen::Matrix<double, 1, 3> N = CurrN.row(fi);
-	//	
-	//	double NxNx = N(0)*N(0);
-	//	double NxNy = N(0)*N(1);
-	//	double NxNz = N(0)*N(2);
-	//	double NyNy = N(1)*N(1);
-	//	double NyNz = N(1)*N(2);
-	//	double NzNz = N(2)*N(2);
-	//	
-	//	double Ne02 = N * e02;
-	//	double Ne21 = N * e21;
-	//	double Ne10 = N * e10;
+		Eigen::Matrix<double, 13, 13> dE_dx = 
+			w2 * 2 * (sqrtE*H_sqrtE + g_sqrtE.transpose()*g_sqrtE);
 
-	//	
-	//	double d_x0Nx = 2 * (Ne02 - Ne10) + 2 * N(0)*(e02(0) - e10(0));
-	//	double d_x0Ny = 2 * N(0)*(e02(1) - e10(1));
-	//	double d_x0Nz = 2 * N(0)*(e02(2) - e10(2));
+		auto pushTriple = [&](int row, int col, double val) {
+			if (row <= col) {
+				II.push_back(row);
+				JJ.push_back(col);
+				SS.push_back(val);
+			}
+		};
+		int startC = 3 * restShapeV.rows() + 3 * restShapeF.rows();
+		int startR = 3 * restShapeV.rows() + 6 * restShapeF.rows();
+		int numV = restShapeV.rows();
+		int numF = restShapeF.rows();
+		for (int xyz1 = 0; xyz1 < 3; ++xyz1) {
+			pushTriple(x0 + xyz1 * numV, startR + fi, dE_dx(0 + xyz1, 12));
+			pushTriple(x1 + xyz1 * numV, startR + fi, dE_dx(3 + xyz1, 12));
+			pushTriple(x2 + xyz1 * numV, startR + fi, dE_dx(6 + xyz1, 12));
+			pushTriple(fi + startC + xyz1 * numF, startR + fi, dE_dx(9 + xyz1, 12));
+			for (int xyz2 = 0; xyz2 < 3; ++xyz2) {
+				pushTriple(x0 + xyz1 * numV, x0 + xyz2 * numV, dE_dx(0 + xyz1, 0 + xyz2));
+				pushTriple(x0 + xyz1 * numV, x1 + xyz2 * numV, dE_dx(0 + xyz1, 3 + xyz2));
+				pushTriple(x0 + xyz1 * numV, x2 + xyz2 * numV, dE_dx(0 + xyz1, 6 + xyz2));
+				pushTriple(x1 + xyz1 * numV, x0 + xyz2 * numV, dE_dx(3 + xyz1, 0 + xyz2));
+				pushTriple(x1 + xyz1 * numV, x1 + xyz2 * numV, dE_dx(3 + xyz1, 3 + xyz2));
+				pushTriple(x1 + xyz1 * numV, x2 + xyz2 * numV, dE_dx(3 + xyz1, 6 + xyz2));
+				pushTriple(x2 + xyz1 * numV, x0 + xyz2 * numV, dE_dx(6 + xyz1, 0 + xyz2));
+				pushTriple(x2 + xyz1 * numV, x1 + xyz2 * numV, dE_dx(6 + xyz1, 3 + xyz2));
+				pushTriple(x2 + xyz1 * numV, x2 + xyz2 * numV, dE_dx(6 + xyz1, 6 + xyz2));
+				pushTriple(fi + startC + xyz1 * numF, fi + startC + xyz2 * numF, dE_dx(9 + xyz1, 9 + xyz2));
+				pushTriple(x0 + xyz1 * numV, fi + startC + xyz2 * numF, dE_dx(0 + xyz1, 9 + xyz2));
+				pushTriple(x1 + xyz1 * numV, fi + startC + xyz2 * numF, dE_dx(3 + xyz1, 9 + xyz2));
+				pushTriple(x2 + xyz1 * numV, fi + startC + xyz2 * numF, dE_dx(6 + xyz1, 9 + xyz2));	
+			}
+		}
+		pushTriple(startR + fi, startR + fi, dE_dx(12, 12));
+	}
+	
 
-	//	double d_y0Nx = 2 * N(1)*(e02(0) - e10(0)); 
-	//	double d_y0Ny = 2 * (Ne02 - Ne10) + 2 * N(1)*(e02(1) - e10(1));
-	//	double d_y0Nz = 2 * N(1)*(e02(2) - e10(2));
-
-	//	double d_z0Nx = 2 * N(2)*(e02(0) - e10(0));
-	//	double d_z0Ny = 2 * N(2)*(e02(1) - e10(1));
-	//	double d_z0Nz = 2 * (Ne02 - Ne10) + 2 * N(2)*(e02(2) - e10(2));
-
-	//	//////////////////////////////////////////
-	//	double d_x1Nx = 2 * (Ne10 - Ne21) + 2 * N(0)*(e10(0) - e21(0));
-	//	double d_x1Ny = 2 * N(0)*(e10(1) - e21(1));
-	//	double d_x1Nz = 2 * N(0)*(e10(2) - e21(2));
-
-	//	double d_y1Nx = 2 * N(1)*(e10(0) - e21(0)); 
-	//	double d_y1Ny = 2 * (Ne10 - Ne21) + 2 * N(1)*(e10(1) - e21(1));
-	//	double d_y1Nz = 2 * N(1)*(e10(2) - e21(2));
-
-	//	double d_z1Nx = 2 * N(2)*(e10(0) - e21(0));
-	//	double d_z1Ny = 2 * N(2)*(e10(1) - e21(1));
-	//	double d_z1Nz = 2 * (Ne10 - Ne21) + 2 * N(2)*(e10(2) - e21(2));
-	//	//////////////////////////////////////////
-	//	double d_x2Nx = 2 * (Ne21 - Ne02) + 2 * N(0)*(e21(0) - e02(0));
-	//	double d_x2Ny = 2 * N(0)*(e21(1) - e02(1));
-	//	double d_x2Nz = 2 * N(0)*(e21(2) - e02(2));
-
-	//	double d_y2Nx = 2 * N(1)*(e21(0) - e02(0));
-	//	double d_y2Ny = 2 * (Ne21 - Ne02) + 2 * N(1)*(e21(1) - e02(1));
-	//	double d_y2Nz = 2 * N(1)*(e21(2) - e02(2));
-
-	//	double d_z2Nx = 2 * N(2)*(e21(0) - e02(0));
-	//	double d_z2Ny = 2 * N(2)*(e21(1) - e02(1));
-	//	double d_z2Nz = 2 * (Ne21 - Ne02) + 2 * N(2)*(e21(2) - e02(2));
-	//	////////////////////////////////////////////////
-	//	double d_NxNx = 2 * e10(0)*e10(0) + 2 * e21(0)*e21(0) + 2 * e02(0)*e02(0);
-	//	double d_NxNy = 2 * e10(1)*e10(0) + 2 * e21(1)*e21(0) + 2 * e02(1)*e02(0);
-	//	double d_NxNz = 2 * e10(2)*e10(0) + 2 * e21(2)*e21(0) + 2 * e02(2)*e02(0);
-
-	//	double d_NyNy = 2 * e10(1)*e10(1) + 2 * e21(1)*e21(1) + 2 * e02(1)*e02(1);
-	//	double d_NyNz = 2 * e10(2)*e10(1) + 2 * e21(2)*e21(1) + 2 * e02(2)*e02(1);
-	//	
-	//	double d_NzNz = 2 * e10(2)*e10(2) + 2 * e21(2)*e21(2) + 2 * e02(2)*e02(2);
-	//	
-	//	Eigen::Matrix<double, 12, 12> dE_dx;
-	//	dE_dx <<
-	//		NxNx * 4, NxNy * 4, NxNz * 4, -2 * NxNx, -2 * NxNy, -2 * NxNz, -2 * NxNx, -2 * NxNy, -2 * NxNz, d_x0Nx, d_x0Ny, d_x0Nz,
-	//		NxNy * 4, NyNy * 4, NyNz * 4, -2 * NxNy, -2 * NyNy, -2 * NyNz, -2 * NxNy, -2 * NyNy, -2 * NyNz, d_y0Nx, d_y0Ny, d_y0Nz,
-	//		NxNz * 4, NyNz * 4, NzNz * 4, -2 * NxNz, -2 * NyNz, -2 * NzNz, -2 * NxNz, -2 * NyNz, -2 * NzNz, d_z0Nx, d_z0Ny, d_z0Nz,
-	//		-2 * NxNx, -2 * NxNy, -2 * NxNz, 4 * NxNx, 4 * NxNy, 4 * NxNz, -2 * NxNx, -2 * NxNy, -2 * NxNz, d_x1Nx, d_x1Ny, d_x1Nz,
-	//		-2 * NxNy, -2 * NyNy, -2 * NyNz, 4 * NxNy, 4 * NyNy, 4 * NyNz, -2 * NxNy, -2 * NyNy, -2 * NyNz, d_y1Nx, d_y1Ny, d_y1Nz,
-	//		-2 * NxNz, -2 * NyNz, -2 * NzNz, 4 * NxNz, 4 * NyNz, 4 * NzNz, -2 * NxNz, -2 * NyNz, -2 * NzNz, d_z1Nx, d_z1Ny, d_z1Nz,
-	//		-2 * NxNx, -2 * NxNy, -2 * NxNz, -2 * NxNx, -2 * NxNy, -2 * NxNz, 4 * NxNx, 4 * NxNy, 4 * NxNz, d_x2Nx, d_x2Ny, d_x2Nz,
-	//		-2 * NxNy, -2 * NyNy, -2 * NyNz, -2 * NxNy, -2 * NyNy, -2 * NyNz, 4 * NxNy, 4 * NyNy, 4 * NyNz, d_y2Nx, d_y2Ny, d_y2Nz,
-	//		-2 * NxNz, -2 * NyNz, -2 * NzNz, -2 * NxNz, -2 * NyNz, -2 * NzNz, 4 * NxNz, 4 * NyNz, 4 * NzNz, d_z2Nx, d_z2Ny, d_z2Nz,
-	//		d_x0Nx, d_y0Nx, d_z0Nx, d_x1Nx, d_y1Nx, d_z1Nx, d_x2Nx, d_y2Nx, d_z2Nx, d_NxNx, d_NxNy, d_NxNz,
-	//		d_x0Ny, d_y0Ny, d_z0Ny, d_x1Ny, d_y1Ny, d_z1Ny, d_x2Ny, d_y2Ny, d_z2Ny, d_NxNy, d_NyNy, d_NyNz,
-	//		d_x0Nz, d_y0Nz, d_z0Nz, d_x1Nz, d_y1Nz, d_z1Nz, d_x2Nz, d_y2Nz, d_z2Nz, d_NxNz, d_NyNz, d_NzNz;
-	//	dE_dx *= w3;
-
-	//	
-	//	for (int xyz1 = 0; xyz1 < 3; ++xyz1) {
-	//		for (int xyz2 = 0; xyz2 < 3; ++xyz2) {
-	//			
-	//			int Grow = fi + (3 * restShapeV.rows()) + (xyz1 * restShapeF.rows());
-	//			int Gcol = fi + (3 * restShapeV.rows()) + (xyz2 * restShapeF.rows());
-	//			if (Grow <= Gcol) {
-	//				II.push_back(Grow);
-	//				JJ.push_back(Gcol);
-	//				SS.push_back(dE_dx(9+xyz1, 9+xyz2));
-	//			}
-
-	//			for (int xi = 0; xi < 3; ++xi) {
-	//				Gcol = fi + (3 * restShapeV.rows()) + (xyz2 * restShapeF.rows());
-	//				Grow = restShapeF(fi, xi) + (xyz1 * restShapeV.rows());
-	//				if (Grow <= Gcol) {
-	//					II.push_back(Grow);
-	//					JJ.push_back(Gcol);
-	//					SS.push_back(dE_dx(3 * xi + xyz1, 9 + xyz2));
-	//				}
-	//			}
-
-	//			for (int xi = 0; xi < 3; ++xi) {
-	//				for (int xj = 0; xj < 3; ++xj) {
-	//					Grow = restShapeF(fi, xi) + (xyz1 * restShapeV.rows());
-	//					Gcol = restShapeF(fi, xj) + (xyz2 * restShapeV.rows());
-	//					if (Grow <= Gcol) {
-	//						II.push_back(Grow);
-	//						JJ.push_back(Gcol);
-	//						SS.push_back(dE_dx(3 * xi + xyz1, 3 * xj + xyz2));
-	//					}
-	//				}
-	//			}		
-	//		}
-	//	}
-	//}
-		
 	II.push_back(restShapeV.size() + 7 * restShapeF.rows() - 1);
 	JJ.push_back(restShapeV.size() + 7 * restShapeF.rows() - 1);
 	SS.push_back(0);
