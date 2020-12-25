2ce8c6c161b3016b82ccc4f0e627d63ed906b6bd
EliasJadon <elias.jadon1@gmail.com>
2020-12-25 16:32:26 +0200

    .

--- libs/STVK.cpp
+++ libs/STVK.cpp
@@ -203,35 +203,74 @@ Cuda::Array<double>* STVK::gradient(Cuda::Array<double>& X, const bool update)
 		strain[1][0] *= 0.5;
 		strain[1][1] *= 0.5;
 
-		Eigen::Matrix<double, 6, 9> dF_dX;
-		Eigen::Matrix<double, 4, 6> dstrain_dF;
-		Eigen::Matrix<double, 1, 4> dE_dJ;
-		dF_dX <<
-			-dXInv.host_arr[fi].x - dXInv.host_arr[fi].z, dXInv.host_arr[fi].x, dXInv.host_arr[fi].z, 0, 0, 0, 0, 0, 0,
-			-dXInv.host_arr[fi].y - dXInv.host_arr[fi].w, dXInv.host_arr[fi].y, dXInv.host_arr[fi].w, 0, 0, 0, 0, 0, 0,
-			0, 0, 0, -dXInv.host_arr[fi].x - dXInv.host_arr[fi].z, dXInv.host_arr[fi].x, dXInv.host_arr[fi].z, 0, 0, 0,
-			0, 0, 0, -dXInv.host_arr[fi].y - dXInv.host_arr[fi].w, dXInv.host_arr[fi].y, dXInv.host_arr[fi].w, 0, 0, 0,
-			0, 0, 0, 0, 0, 0, -dXInv.host_arr[fi].x - dXInv.host_arr[fi].z, dXInv.host_arr[fi].x, dXInv.host_arr[fi].z,
-			0, 0, 0, 0, 0, 0, -dXInv.host_arr[fi].y - dXInv.host_arr[fi].w, dXInv.host_arr[fi].y, dXInv.host_arr[fi].w;
+		double dF_dX[6][9] = { 0 };
+		dF_dX[0][0] = -dXInv.host_arr[fi].x - dXInv.host_arr[fi].z;
+		dF_dX[0][1] = dXInv.host_arr[fi].x;
+		dF_dX[0][2] = dXInv.host_arr[fi].z; 
 
-		dstrain_dF <<
-			F[0][0], 0, F[1][0], 0, F[2][0], 0,
-			0.5*F[0][1], 0.5*F[0][0], 0.5*F[1][1], 0.5*F[1][0], 0.5*F[2][1], 0.5*F[2][0],
-			0.5*F[0][1], 0.5*F[0][0], 0.5*F[1][1], 0.5*F[1][0], 0.5*F[2][1], 0.5*F[2][0],
-			0, F[0][1], 0, F[1][1], 0, F[2][1];
+		dF_dX[1][0] = -dXInv.host_arr[fi].y - dXInv.host_arr[fi].w;
+		dF_dX[1][1] = dXInv.host_arr[fi].y;
+		dF_dX[1][2] = dXInv.host_arr[fi].w;
+
+		dF_dX[2][3] = -dXInv.host_arr[fi].x - dXInv.host_arr[fi].z;
+		dF_dX[2][4] = dXInv.host_arr[fi].x;
+		dF_dX[2][5] = dXInv.host_arr[fi].z; 
+							
+		dF_dX[3][3] = -dXInv.host_arr[fi].y - dXInv.host_arr[fi].w;
+		dF_dX[3][4] = dXInv.host_arr[fi].y;
+		dF_dX[3][5] = dXInv.host_arr[fi].w;
+				
+		dF_dX[4][6] = -dXInv.host_arr[fi].x - dXInv.host_arr[fi].z;
+		dF_dX[4][7] = dXInv.host_arr[fi].x;
+		dF_dX[4][8] = dXInv.host_arr[fi].z;
+				
+		dF_dX[5][6] = -dXInv.host_arr[fi].y - dXInv.host_arr[fi].w;
+		dF_dX[5][7] = dXInv.host_arr[fi].y;
+		dF_dX[5][8] = dXInv.host_arr[fi].w;
+
+		double dstrain_dF[4][6] = { 0 };
+		dstrain_dF[0][0] = F[0][0];
+		dstrain_dF[0][2] = F[1][0];
+		dstrain_dF[0][4] = F[2][0];
+			
+		dstrain_dF[1][0] = 0.5 * F[0][1];
+		dstrain_dF[1][1] = 0.5 * F[0][0];
+		dstrain_dF[1][2] = 0.5 * F[1][1];
+		dstrain_dF[1][3] = 0.5 * F[1][0];
+		dstrain_dF[1][4] = 0.5 * F[2][1];
+		dstrain_dF[1][5] = 0.5 * F[2][0];
+			
+		dstrain_dF[2][0] = 0.5 * F[0][1];
+		dstrain_dF[2][1] = 0.5 * F[0][0];
+		dstrain_dF[2][2] = 0.5 * F[1][1];
+		dstrain_dF[2][3] = 0.5 * F[1][0];
+		dstrain_dF[2][4] = 0.5 * F[2][1];
+		dstrain_dF[2][5] = 0.5 * F[2][0];
+			
+		dstrain_dF[3][1] = F[0][1];
+		dstrain_dF[3][3] = F[1][1];
+		dstrain_dF[3][5] = F[2][1];
 		
-		dE_dJ <<
-			2 * shearModulus * strain[0][0] + bulkModulus * (strain[0][0] + strain[1][1]),
-			2 * shearModulus * strain[0][1],
-			2 * shearModulus * strain[1][0],
-			2 * shearModulus * strain[1][1] + bulkModulus * (strain[0][0] + strain[1][1]);
-		dE_dJ *= restShapeArea[fi];
-	
-		Eigen::Matrix<double, 1, 9> dE_dX = dE_dJ * dstrain_dF * dF_dX;
+		double dE_dJ[1][4];
+		dE_dJ[0][0] = restShapeArea[fi] * (2 * shearModulus * strain[0][0] + bulkModulus * (strain[0][0] + strain[1][1]));
+		dE_dJ[0][1] = restShapeArea[fi] * (2 * shearModulus * strain[0][1]);
+		dE_dJ[0][2] = restShapeArea[fi] * (2 * shearModulus * strain[1][0]);
+		dE_dJ[0][3] = restShapeArea[fi] * (2 * shearModulus * strain[1][1] + bulkModulus * (strain[0][0] + strain[1][1]));
 		
-		for (int vi = 0; vi < 3; vi++)
-			for (int xyz = 0; xyz < 3; xyz++)
-				grad.host_arr[restShapeF(fi, vi) + (xyz*restShapeV.rows())] += dE_dX[xyz*3 + vi];
+		double dE_dX[1][9];
+		double temp[1][6];
+		multiply<1, 4, 6>(dE_dJ, dstrain_dF, temp);
+		multiply<1, 6, 9>(temp, dF_dX, dE_dX);
+
+		grad.host_arr[v0i + mesh_indices.startVx] += dE_dX[0][0];
+		grad.host_arr[v1i + mesh_indices.startVx] += dE_dX[0][1];
+		grad.host_arr[v2i + mesh_indices.startVx] += dE_dX[0][2];
+		grad.host_arr[v0i + mesh_indices.startVy] += dE_dX[0][3];
+		grad.host_arr[v1i + mesh_indices.startVy] += dE_dX[0][4];
+		grad.host_arr[v2i + mesh_indices.startVy] += dE_dX[0][5];
+		grad.host_arr[v0i + mesh_indices.startVz] += dE_dX[0][6];
+		grad.host_arr[v1i + mesh_indices.startVz] += dE_dX[0][7];
+		grad.host_arr[v2i + mesh_indices.startVz] += dE_dX[0][8];
 	}
 	Cuda::MemCpyHostToDevice(grad);
 	return &grad;
